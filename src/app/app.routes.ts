1322ba3a43c432c81c47671171ecf9cb1d28b9ac
milton2701 <miltonelectronic27@gmail.com>
2025-11-12 10:18:38 -0500

    Primeras vistas Front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { AgregarEntidadFinanciera } from './components/agregar-entidad-financiera/agregar-entidad-financiera';
+import { DetalleCliente } from './components/detalle-cliente/detalle-cliente';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login', component: Login
+  },
+  {
+    path: 'agregar-entidad-financiera', component: AgregarEntidadFinanciera
+  },
+  {
+    path: 'info', component: DetalleCliente
+  }
+]
---
+++ src/app/components/agregar-entidad-financiera/agregar-entidad-financiera.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-entidad-financiera',
+  imports: [],
+  templateUrl: './agregar-entidad-financiera.html',
+  styleUrl: './agregar-entidad-financiera.css',
+})
+export class AgregarEntidadFinanciera {
+
+}
